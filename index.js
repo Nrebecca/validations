0ff375d1e5251bf38c3acd8a9186e4fd9e153842
Rebecca <nzamuyerebecca@gmail.com>
2022-10-18 17:16:11 +0200

    form validations

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+function signup(){
+    var output1 = document.getElementById("output1");
+    var output2 = document.getElementById("output2");
+    output1.innerHTML="please enter your name";
+    output2.innerHTML="please enter your email";
+    var output1 = document.querySelector("#output1");
+    output1.style.color="red";
+    var output2 = document.querySelector("#output2");
+    output2.style.color="red";
+
+}
